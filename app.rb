3233b3852c77850f4b8f80d0c95dcc09b7cf6baa
Liu Yuyu <shinxjl+github@gmail.com>
2012-05-02 19:10:49 +0800

    fix URL redirecting

--- app.rb
+++ app.rb
@@ -1,4 +1,3 @@
-# coding: utf-8
 require 'sinatra'
 require 'haml'
 require 'net/http'
@@ -27,19 +26,45 @@ post '/trans' do
   path = uri.path
   file_name = path[path.rindex("/") + 1..-1]
   
-
   headers \
     "Content-type"   => "application/octet-stream",
     "Content-Disposition" => "attachment; filename=\"#{file_name}\"",
     "Cache-Control" => "no-cache, must-revalidate",
     "Pragma" => "no-cache"
-  
+
   stream do |out|
-    Net::HTTP::Proxy(settings.proxy_config['addr'], settings.proxy_config['port'], settings.proxy_config['user'], settings.proxy_config['pass']).start(uri.host, uri.port) do |http|
-      http.get(uri.full_path) do |str|
-        out << str
+    do_transfer(out, uri)
+  end
+end
+
+def do_transfer(out, uri, limit = 10)
+  location = nil
+  error = nil
+  if limit == 0
+    error = 'too many HTTP redirects'
+  else
+    begin
+      Net::HTTP::Proxy(settings.proxy_config['addr'], settings.proxy_config['port'], settings.proxy_config['user'], settings.proxy_config['pass']).start(uri.host, uri.port) do |http|
+        http.request_get(uri.full_path) do |resp|
+          case resp
+          when Net::HTTPSuccess then
+            resp.read_body { |segment| out << segment }
+          when Net::HTTPRedirection then
+            location = resp['location']
+          else
+            error = resp.value
+          end
+        end
       end
+    rescue => exception
+      error = exception.to_s
     end
   end
   
+  unless location.nil?
+    do_transfer(out, URI.parse(location), limit - 1)
+  end
+  if error
+    out << error
+  end
 end
